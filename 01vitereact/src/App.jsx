ea91046ea01d0bcabd7884eb33b77b6af0e41c3c
ayanahmad11 <ayan2011032@akgec.ac.in>
2024-05-19 14:16:14 +0530

    custom-react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,24 @@
+
+import Bread from "./Bread"
+
+function App() {
+  
+  return (
+   <div>
+   
+    <h1>
+      chai aur react with vite | HC
+    </h1>
+    <Bread>
+    </Bread>
+    <p>
+      take your snack at 5:00 pm
+    </p>
+
+   </div>
+   
+  )
+
+}
+
+export default App
---
+++ 01vitereact/src/Bread.jsx
@@ -0,0 +1,9 @@
+function Bread(){
+    return (
+        <h3>
+            Bread makkhan is ready.
+        </h3>
+            )
+
+}
+export default Bread;
