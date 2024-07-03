4f9341aac5f86a726e0e237e37592572c1946ab8
ayanahmad11 <154846395+ayanahmad11@users.noreply.github.com>
2024-07-03 08:46:45 +0530

    Update App.jsx

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -10,70 +10,68 @@ const App = () => {
   const [convertedAmount, setConvertedAmount] = useState(0);
   const currencyInfo = useCurrencyInfo(from);
   const currencyOptions = Object.keys(currencyInfo);
+
   const Swap = () => {
     setAmount(0);
     setConvertedAmount(0);
     setTo(from);
     setFrom(to);
   };
+
   const Convert = () => {
     setConvertedAmount(amount * (currencyInfo[to] || 0));
   };
+
   useEffect(() => {
     Convert();
   }, [amount, to, currencyInfo]);
+
   return (
-    <>
     <div
-      className="w-screen h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      className="w-screen h-screen flex justify-center items-center bg-cover bg-no-repeat bg-fixed"
       style={{
         backgroundImage: `url('https://images.pexels.com/photos/164527/pexels-photo-164527.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
-    }}
+      }}
     >
-    <div className="w-full">
-    <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
-            <h1 className="text-center text-2xl font-bold">Currency Converter</h1>
-            <form>
-              <div className="w-full mb-1">
-                <InputBox
-                  label="From"
-                  amount={amount}
-                  currencyOptions={currencyOptions}
-                  onCurrencyChange={(currency) => setFrom(currency)}
-                  selectCurrency={from}
-                  onAmountChange={(amount) => {
-                    if (amount < 0) return;
-                    setAmount(amount);
-                  }}
-                />
-              </div>
-              <div className="relative w-full h-0.5">
-                <button
-                  type="button"
-                  className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-lg bg-blue-600 text-white p-2"
-                  onClick={Swap}
-                >
-                     Swap
-                </button>
-              </div>
-              <div className="w-full mt-1 mb-4">
-                <InputBox
-                  label="To"
-                  amount={convertedAmount}
-                  currencyOptions={currencyOptions}
-                  onCurrencyChange={(currency) => setTo(currency)}
-                  selectCurrency={to}
-                  amountDisable
-                />
-              </div>
-            </form>
+      <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+        <h1 className="text-center text-2xl font-bold mb-4">Currency Converter</h1>
+        <form>
+          <div className="w-full mb-1">
+            <InputBox
+              label="From"
+              amount={amount}
+              currencyOptions={currencyOptions}
+              onCurrencyChange={(currency) => setFrom(currency)}
+              selectCurrency={from}
+              onAmountChange={(amount) => {
+                if (amount < 0) return;
+                setAmount(amount);
+              }}
+            />
           </div>
-        </div>
-        </div>
-    </>
+          <div className="relative w-full h-0.5">
+            <button
+              type="button"
+              className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-lg bg-blue-600 text-white p-2"
+              onClick={Swap}
+            >
+              Swap
+            </button>
+          </div>
+          <div className="w-full mt-1 mb-4">
+            <InputBox
+              label="To"
+              amount={convertedAmount}
+              currencyOptions={currencyOptions}
+              onCurrencyChange={(currency) => setTo(currency)}
+              selectCurrency={to}
+              amountDisable
+            />
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
 export default App;
-                          
-
